338bdd2961bdfd499bd7cce128f083ecee5d1fea
Martik-k <kosiv.marta@gmail.com>
2025-11-28 23:26:53 +0200

    Added fourth task

--- task4/main.cpp
+++ task4/main.cpp
@@ -1,3 +1,19 @@
-//
-// Created by marta on 11/28/25.
-//
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+int length_min_row() {
+    std::string line;
+    getline(std::cin, line);
+    std::unordered_set<char> letters;
+
+    for (char& c : line) {
+        if (c != ' ') letters.insert(c);
+    }
+    return static_cast<int>(letters.size());
+}
+
+int main() {
+    std::cout << length_min_row() << std::endl;
+    return 0;
+}
